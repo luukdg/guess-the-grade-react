896e7a495c9f17dbe03d5898de7838f0dc457858
luukdg <luukdegraaf1993@gmail.com>
2025-10-27 17:09:04 +0100

    Implemented the Vite PWA plugin.

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,37 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        id: "/guess-the-grade-react/",
+        name: "Guess the Grade",
+        short_name: "GuessGrade",
+        start_url: ".",
+        display: "standalone",
+        background_color: "oklch(0.13 0.028 261.692)",
+        icons: [
+          {
+            src: "/guess-the-grade-react/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/guess-the-grade-react/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/guess-the-grade-react",
   resolve: {
     alias: {
