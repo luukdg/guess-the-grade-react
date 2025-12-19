c91b70c1f4925e625711ead4effc7b9e4bb386c6
luukdg <luukdegraaf1993@gmail.com>
2025-12-19 17:38:33 +0100

    Updates

--- assets/react-JGkf-ydW.js
+++ assets/react-JGkf-ydW.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-BuLkrMe0.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-Cp2x8l1r.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-IhOkJeQ6.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-Cp2x8l1r.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
