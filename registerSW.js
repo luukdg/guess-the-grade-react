6672e56bb300e991455f1ac8fa70987b6da19062
luukdg <luukdegraaf1993@gmail.com>
2026-01-23 17:40:26 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/guess-the-grade-react/sw.js', { scope: '/guess-the-grade-react/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
